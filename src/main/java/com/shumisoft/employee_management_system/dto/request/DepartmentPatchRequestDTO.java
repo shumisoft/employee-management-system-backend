f9b6c7c89111e22bcfc3aa5e5ad2c4b8628314d0
Ritwikrajsingh <ritwikrajdhangta@gmail.com>
2024-10-02 13:55:20 +0530

    feat: created DepartmentPatchRequestDTO

---
+++ src/main/java/com/shumisoft/employee_management_system/dto/request/DepartmentPatchRequestDTO.java
@@ -0,0 +1,9 @@
+package com.shumisoft.employee_management_system.dto.request;
+
+import lombok.Getter;
+
+@Getter
+public class DepartmentPatchRequestDTO {
+    private String name;
+    private String description;
+}
