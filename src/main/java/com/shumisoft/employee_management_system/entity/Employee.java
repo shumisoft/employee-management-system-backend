90e8bf32b6c8f022d94fc482c0dab7dea2c892ea
Ritwikrajsingh <ritwikrajdhangta@gmail.com>
2024-10-10 23:17:43 +0530

    feat: created Employee entity with fields, UUID ID, and Status enum for persistence

---
+++ src/main/java/com/shumisoft/employee_management_system/entity/Employee.java
@@ -0,0 +1,42 @@
+package com.shumisoft.employee_management_system.entity;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Builder
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Employee {
+
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    @Column(nullable = false)
+    private String firstName;
+    private String lastName;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+    private String phone;
+    private LocalDate joinDate;
+    private Status status;
+
+    // Enum for Status
+    public enum Status {
+        ACTIVE,
+        INACTIVE
+    }
+
+}
