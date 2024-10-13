d36c73b08542f1d88bb63849e1fed0d671fb19dd
Ritwikrajsingh <ritwikrajdhangta@gmail.com>
2024-10-13 23:26:29 +0530

    feat: created EmployeeResponseDTO with fromEntity mapper

---
+++ src/main/java/com/shumisoft/employee_management_system/dto/response/EmployeeResponseDTO.java
@@ -0,0 +1,31 @@
+package com.shumisoft.employee_management_system.dto.response;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+import com.shumisoft.employee_management_system.entity.Employee;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class EmployeeResponseDTO {
+
+    private UUID id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String phone;
+    private LocalDate joinDate;
+    private Employee.Status status;
+
+    public static EmployeeResponseDTO fromEntity(Employee entity) {
+
+        return EmployeeResponseDTO.builder().id(entity.getId()).firstName(entity.getFirstName())
+                .lastName(entity.getLastName()).email(entity.getEmail()).phone(entity.getPhone())
+                .joinDate(entity.getJoinDate()).status(entity.getStatus()).build();
+
+    }
+
+}
