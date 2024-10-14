2effea90d7854271744b079f2afe982ac5485277
Ritwikrajsingh <ritwikrajdhangta@gmail.com>
2024-10-14 23:27:20 +0530

    feat: created EmployeePatchRequestDTO with isEmpty check for partial updates

---
+++ src/main/java/com/shumisoft/employee_management_system/dto/request/EmployeePatchRequestDTO.java
@@ -0,0 +1,30 @@
+package com.shumisoft.employee_management_system.dto.request;
+
+import java.time.LocalDate;
+
+import com.shumisoft.employee_management_system.entity.Employee;
+
+import lombok.Getter;
+
+@Getter
+public class EmployeePatchRequestDTO {
+
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String phone;
+    private LocalDate joinDate;
+    private Employee.Status status;
+
+    public boolean isEmpty() {
+
+        return firstName == null &&
+                lastName == null &&
+                email == null &&
+                phone == null &&
+                joinDate == null &&
+                status == null;
+
+    }
+
+}
