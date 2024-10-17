74117b0a77aa0c13e7d20fa0d136cc3b1abaf294
Ritwikrajsingh <ritwikrajdhangta@gmail.com>
2024-10-17 23:33:21 +0530

    feat: created EmployeeController with CRUD and patch endpoints for employee management

---
+++ src/main/java/com/shumisoft/employee_management_system/controller/EmployeeController.java
@@ -0,0 +1,82 @@
+package com.shumisoft.employee_management_system.controller;
+
+import java.util.UUID;
+
+import org.springframework.data.domain.Page;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.shumisoft.employee_management_system.dto.request.EmployeePatchRequestDTO;
+import com.shumisoft.employee_management_system.dto.request.EmployeeRequestDTO;
+import com.shumisoft.employee_management_system.dto.response.EmployeeResponseDTO;
+import com.shumisoft.employee_management_system.service.impl.EmployeeServiceImpl;
+
+import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
+
+@RestController
+@RequestMapping("api/employee")
+@AllArgsConstructor
+public class EmployeeController {
+
+    private final EmployeeServiceImpl service;
+
+    @PostMapping
+    public ResponseEntity<EmployeeResponseDTO> createEmployee(@Valid @RequestBody EmployeeRequestDTO dto) {
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(service.createEmployee(dto));
+
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<EmployeeResponseDTO>> getAllEmployees(
+            @RequestParam(defaultValue = "0") Integer page,
+            @RequestParam(defaultValue = "10") Integer pageSize) {
+
+        return ResponseEntity.ok(service.getAllEmployees(page, pageSize));
+
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<EmployeeResponseDTO> getEmployeeById(@PathVariable UUID id) {
+
+        return ResponseEntity.ok(service.getEmployeeById(id));
+
+    }
+
+    @PutMapping("{id}")
+    public ResponseEntity<EmployeeResponseDTO> updateEmployeeById(@PathVariable UUID id,
+            @RequestBody EmployeeRequestDTO dto) {
+
+        return ResponseEntity.ok(service.updateEmployeeById(id, dto));
+
+    }
+
+    @PatchMapping("{id}")
+    public ResponseEntity<EmployeeResponseDTO> patchEmployeeById(@PathVariable UUID id,
+            @RequestBody EmployeePatchRequestDTO dto) {
+
+        return ResponseEntity.ok(service.patchEmployeeById(id, dto));
+
+    }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity<Void> deleteEmployeeById(@PathVariable UUID id) {
+
+        service.deleteEmployeeById(id);
+
+        return ResponseEntity.noContent().build();
+
+    }
+
+}
