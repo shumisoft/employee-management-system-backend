f89dcf26534cb682fcd12b25df49cb4b301dfe28
Ritwikrajsingh <ritwikrajdhangta@gmail.com>
2024-10-25 04:37:25 +0530

    fix: CORS error on '/api/employee' routes

--- src/main/java/com/shumisoft/employee_management_system/controller/EmployeeController.java
+++ src/main/java/com/shumisoft/employee_management_system/controller/EmployeeController.java
@@ -5,6 +5,7 @@ import java.util.UUID;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -26,6 +27,7 @@ import lombok.AllArgsConstructor;
 
 @RestController
 @RequestMapping("api/employee")
+@CrossOrigin(origins = "*")
 @AllArgsConstructor
 public class EmployeeController {
 
