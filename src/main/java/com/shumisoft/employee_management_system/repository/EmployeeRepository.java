87f95788155e4aaeec8249bcfb7aafbd2a826103
Ritwikrajsingh <ritwikrajdhangta@gmail.com>
2024-10-30 21:52:41 +0530

    feat: add paginated findByDepartmentId method to EmployeeRepository

--- src/main/java/com/shumisoft/employee_management_system/repository/EmployeeRepository.java
+++ src/main/java/com/shumisoft/employee_management_system/repository/EmployeeRepository.java
@@ -2,6 +2,8 @@ package com.shumisoft.employee_management_system.repository;
 
 import java.util.UUID;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,5 +11,7 @@ import com.shumisoft.employee_management_system.entity.Employee;
 
 @Repository
 public interface EmployeeRepository extends JpaRepository<Employee, UUID> {
+
+    Page<Employee> findByDepartmentId(Integer departmentId, Pageable pageable);
 
 }
