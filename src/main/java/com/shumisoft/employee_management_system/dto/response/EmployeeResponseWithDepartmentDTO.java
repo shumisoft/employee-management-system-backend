928aff457a409513c97aa5e5b0fc093bb7cd260e
Ritwikrajsingh <ritwikrajdhangta@gmail.com>
2024-11-26 14:04:21 +0530

    feat: enriched EmployeeResponseWithDepartmentDTO with manager details

--- src/main/java/com/shumisoft/employee_management_system/dto/response/EmployeeResponseWithDepartmentDTO.java
+++ src/main/java/com/shumisoft/employee_management_system/dto/response/EmployeeResponseWithDepartmentDTO.java
@@ -21,12 +21,14 @@ public class EmployeeResponseWithDepartmentDTO {
     private LocalDate joinDate;
     private Employee.Status status;
     private Department department;
+    private ManagerDTO manager;
 
     public static EmployeeResponseWithDepartmentDTO fromEntity(Employee entity) {
 
         return EmployeeResponseWithDepartmentDTO.builder().id(entity.getId()).firstName(entity.getFirstName())
                 .lastName(entity.getLastName()).email(entity.getEmail()).phone(entity.getPhone())
-                .joinDate(entity.getJoinDate()).status(entity.getStatus()).department(entity.getDepartment()).build();
+                .joinDate(entity.getJoinDate()).status(entity.getStatus()).department(entity.getDepartment())
+                .manager(ManagerDTO.fromEntity(entity.getManager())).build();
 
     }
 
