5dcc0cb17623a233952cde12e0077ce2dcc8b91f
Ritwikrajsingh <ritwikrajdhangta@gmail.com>
2025-08-17 21:09:48 +0530

    added added sercurity config and IP Logger

---
+++ src/main/java/com/shumisoft/employee_management_system/config/IPLoggerFilter.java
@@ -0,0 +1,22 @@
+package com.shumisoft.employee_management_system.config;
+
+import java.io.IOException;
+
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class IPLoggerFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+
+        System.out.println("remote address: " + request.getRemoteAddr());
+        filterChain.doFilter(request, response);
+    }
+
+}
